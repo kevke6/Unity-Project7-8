af516c1255c584f4f4b4e827d9a91a8e80e03fe8
0888296 <5314zain@gmail.com>
2016-05-17 12:02:55 +0200

    Movement/camera
    Movement 1/2 en camera is gelukt

---
+++ Assets/Standard Assets/CrossPlatformInput/Scripts/CameraGUI.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraGUI : MonoBehaviour {
+
+	public float rotY;
+	public float rotX;
+	private float sensitivity = 2.0f;
+	// Use this for initialization
+	void Start () {
+		rotY = transform.localEulerAngles.y;
+		rotX = transform.localEulerAngles.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		rotX += Input.GetAxis ("Mouse X") * sensitivity;
+		rotX = Mathf.Clamp (rotX,-180,180);
+		rotY += Input.GetAxis ("Mouse Y") * sensitivity;
+		rotY = Mathf.Clamp (rotY,-70,70);
+
+		transform.localEulerAngles = new Vector3 (-rotY, rotX, transform.localEulerAngles.z);
+	}
+}
---
+++ Assets/Standard Assets/Environment/Water/Water/Scripts/ShipMovement.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipMovement : MonoBehaviour {
+
+	private int accel =20;
+	private int turn = 50;
+	private float speedForward =1;
+	private float speedTurn =1;
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKey ("w")) 
+		{
+			//SetTransformZ((transform.position.z) + speedForward / accel);
+			transform.TransformDirection(Vector3.forward);
+		}
+		if (Input.GetKey ("s")) 
+		{
+			SetTransformZ((transform.position.z) - speedForward / accel);
+		}
+		if (Input.GetKey ("a")) 
+		{
+			transform.Rotate (Vector3.down * Time.deltaTime);
+		}
+		if (Input.GetKey ("d")) 
+		{
+			transform.Rotate (Vector3.up * Time.deltaTime*2);
+		}
+	}
+
+	void SetTransformZ(float n)
+	{
+		transform.position = new Vector3(transform.position.x, transform.position.y, n);
+	}
+}
