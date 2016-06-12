ec149bb7ca4a4260ca756b389608735b4c31eee2
Michel <michelv1994@caiway.net>
2016-06-12 19:21:40 +0200

    Second Input
    Game Over still not working :-1:

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	private bool gameOver;
+	private bool restart;
+
+	// Use this for initialization
+	void Start () {
+		gameOver = false;
+		restart = false;
+		Time.timeScale = 1.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.R)) {
+			Application.LoadLevel (Application.loadedLevel);
+		}
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			Time.timeScale = 0.0f;
+			LoadOnClick loadPause = new LoadOnClick ();
+			loadPause.LoadScene (0);
+		}
+	}
+
+	public void GameOver() {
+		gameOver = true;
+		if (gameOver) {
+			restart = true;
+			Time.timeScale = 0.0f;
+			LoadOnClick loadOnClick = new LoadOnClick ();
+			loadOnClick.LoadScene (3);
+		}
+	}
+}
---
+++ Assets/Scripts/GameOverByContact.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOverByContact : MonoBehaviour {
+
+	private GameController gameController;
+	private string objectHit;
+
+	void Start () {
+		GameObject gameControllerObject = GameObject.FindWithTag ("GameController");
+		if (gameControllerObject != null) {
+			gameController = gameControllerObject.GetComponent<GameController> ();
+		}
+		if (gameController == null) {
+			Debug.Log ("Cannot find 'GameController' script");
+		}
+	}
+
+
+	void OnTriggerEnter (Collider col) {
+		if (col.tag == "Harbor") {
+			Debug.Log("You hit the harbor");
+			gameController.GameOver ();
+		}
+	}
+}
